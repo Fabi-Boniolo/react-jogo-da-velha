9fda84d17583348b71ce33a23fc202861ab1fdd8
Fabi-Boniolo <fabianaboniolo@gmail.com>
2024-02-02 15:50:59 -0300

    Criado o componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from './Header.module.css'
 
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
+import Icon from "../icon/Icon"
 
 function Header() {
     return (
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
             <Subtitle>Criado por Fabi Boniolo</Subtitle>
+            <div className= {styles.iconContent}>
+                <Icon iconName="github"  link="https://github.com/fabi-boniolo"/>
+            </div>
         </div>
 
     )
